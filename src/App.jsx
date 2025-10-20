67585eb8eaf5f6aea10db78acc3ceb31872bc6b5
hgdav <aes.hur.v@gmail.com>
2025-10-20 16:42:24 -0500

    drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,12 @@ export default function App() {
   const [statusMap, setStatusMap] = useState({});
 
   const sensors = useSensors(
-    useSensor(PointerSensor),
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        delay: 250,
+        tolerance: 5,
+      },
+    }),
     useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
   );
 
